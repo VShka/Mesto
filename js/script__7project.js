b365707e93bb3fab1820ff11ef3128074b19472a
Viktor <viktor.shkarupa@yandex.ru>
2019-12-17 00:46:08 +0300

    небольшие доработки

--- js/script__7project.js
+++ js/script__7project.js
@@ -1,8 +1,16 @@
 /* Переменные */
 
+// форма редактирования профиля
 const popupEditProfileBtn = profile.querySelector('.edit-profile__button');
 const popupEditProfile = document.querySelector('.popup__edit-profile');
 const closeEditProfile = document.querySelector('.popup__close_edit-profile');
+
+// данные полей формы
+const userInfo = profile.querySelector('.user-info');
+const userInfoName = userInfo.querySelector('.user-info__name');
+const userInfoJob = userInfo.querySelector('.user-info__job');
+const popupEditForm = document.forms.edit;
+
 
 
 /* Функции */
@@ -11,9 +19,10 @@ const closeEditProfile = document.querySelector('.popup__close_edit-profile');
 // открывает и закрывает форму редактирования профиля
 const openAndCloseEditForm = function () {
     popupEditProfile.classList.toggle('popup_is-opened');
-  };
+};
 
 /* События */
+
 
 // открытие и закрытие формы
 popupEditProfileBtn.addEventListener('click', openAndCloseEditForm);
