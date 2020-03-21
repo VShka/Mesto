be2c8d76b639d740735628ead8acbfedda1322ed
Viktor <viktor.shkarupa@yandex.ru>
2020-03-21 18:15:16 +0300

    рефакторинг
    рефактор функции для добавления 1 карточки через форму

--- js/CardList.js
+++ js/CardList.js
@@ -15,12 +15,29 @@ class CardList {
         this.addCard(card.name, card.link);
       })
     }
+    
 }
 // экземпляр контейнера 
 const cardList = new CardList(placesList, card);
 
 // передаем методу массив карточек и отрисовываем при загрузке в нашем контейнере
 cardList.render(initialCards);
+
+
+popupForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = formName.value;
+  const link = formLink.value;
+
+  cardList.addCard(name, link);
+
+  popup.classList.remove('popup_is-opened');
+  popupForm.reset();
+
+  btnAddPlace.setAttribute('disabled', true);
+  btnAddPlace.classList.add('popup__button_disabled');
+});
 
 // лайк и удаление карточки
 cardList.container.addEventListener('click', event => {
--- js/script.js
+++ js/script.js
@@ -13,37 +13,6 @@ const btnAddPlace = document.querySelector('.btn__add-place');
 
 /* Функции */
 
-
-/* 1. Десять карточек «из коробки» */
-
-// функция возвращает разметку карточки, которую впоследствии будем отрисовывать при загрузке страницы 
-// const getImgCard = function (obj) {
-//   return `<div class="place-card">
-//                 <div class="place-card__image" style="background-image: url(${obj.link})">
-//                     <button class="place-card__delete-icon"></button>
-//                 </div>
-//                 <div class="place-card__description">
-//                     <h3 class="place-card__name">${obj.name}</h3>
-//                     <button class="place-card__like-icon"></button>
-//                 </div>
-//           </div>`;
-// };
-
-// // отрисовка разметки карточки в html
-// const renderCard = function (obj) {
-//   const template = getImgCard(obj);
-//   placesList.insertAdjacentHTML('beforeend', template);
-// };
-
-// // перебор по массиву с коллбеком, который берет значения каждого элемента массива и отрисовывает карточки
-// // при загрузке страницы
-// const initialCardsToDisplay = function (arrCards) {
-//     arrCards.forEach(function (card) {
-//       renderCard(card);
-//     })
-// };
-// initialCardsToDisplay(initialCards);
-
 /* 2. Открытие формы и 3. Закрытие формы */
 
 // переключает класс и собственно открывает или закрывает форму
@@ -55,23 +24,6 @@ const closeFormEsc = function (event) {
     popup.classList.remove('popup_is-opened');
   }
 };
-
-/* 5. Добавление карточки */
-
-// используем функцию getImgCard и добавляем элемент
-// const addCard = function (event, doneField) {
-//   event.preventDefault();
-
-//   const formName = popupForm.elements.name;
-//   const formLink = popupForm.elements.link;
-
-//   doneField({ name: formName.value, link: formLink.value });
-//   popup.classList.remove('popup_is-opened');
-//   popupForm.reset();
-
-//   btnAddPlace.setAttribute('disabled', true);
-//   btnAddPlace.classList.add('popup__button_disabled');
-// };
 
 
 /* Валидация кнопки*/
@@ -92,11 +44,6 @@ const disableBtnForm = function() {
 openPopup.addEventListener('click', openAndCloseForm);
 closePopup.addEventListener('click', openAndCloseForm);
 document.addEventListener('keydown', closeFormEsc);
-
-// добавление карточки
-popupForm.addEventListener('submit', function (event) {
-  addCard(event, renderCard);
-}) 
 
 // активация и дезактивация кнопки -------------
 popupForm.addEventListener('input', disableBtnForm);
